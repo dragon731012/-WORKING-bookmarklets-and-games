906c21cb7c0732e223468ca08240b1aed15bbbe6
dragon731012 <105754685+dragon731012@users.noreply.github.com>
2022-11-15 15:04:07 -0600

    Update test(DOES NOT WORK).js

--- test(DOES NOT WORK).js
+++ test(DOES NOT WORK).js
@@ -1,1 +1,1 @@
-javascript:var a=prompt("What bookmarklet would you like to activate? click=1 color=2 cloak=3 scroll=4 BAC=5 bypass=6 others=7"); (function() { if (a == 1) {alert("hi1")} else if (b==2) {alert("hi2")};a(document);} else if (a ==3) {alert("3"}})();
+javascript:var a=prompt("What bookmarklet would you like to activate? click=1 color=2 cloak=3 scroll=4 BAC=5 bypass=6 others=7"); (function() { if (a == 1) {} else if (b==2) {};a(document);}else if (b ==3) {alert("3)"}})();
