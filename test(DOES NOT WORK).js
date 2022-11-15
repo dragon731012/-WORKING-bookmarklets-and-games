7d2aebe3b176da7867b8d4ad52ab4ce21fd372b3
dragon731012 <105754685+dragon731012@users.noreply.github.com>
2022-11-15 12:14:06 -0600

    Update test(DOES NOT WORK).js

--- test(DOES NOT WORK).js
+++ test(DOES NOT WORK).js
@@ -1,2 +1,7 @@
-javascript:var a=1;if (a==1) {alert("hi"}
-
+javascript:var one = prompt("Enter the first number");var two = prompt("Enter the second number");one = parseInt(one);two = parseInt(two);
+		if (one == two)
+			document.write(one + " is equal to " + two + ".");
+		else if (one<two)
+			document.write(one + " is less than " + two + ".");
+		else
+			document.write(one + " is greater than " + two + ".");
