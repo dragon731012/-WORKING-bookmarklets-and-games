e15a9d7c54fdb69e49c787daa775699dda6702a2
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-22 12:11:46 -0600

    Update Game1test.js

--- tests (don't work)/Game1test.js
+++ tests (don't work)/Game1test.js
@@ -1,11 +1,18 @@
 javascript:
 var start = confirm("Would you like to play Untiteld Game");
 if(start == true){
-var random = Math.floor(Math.random() * 11) + 1;
-alert(random);
+var truthordare = confirm("Truth or Dare, Cancel for Truth, Okay for Dare")
 }
 else{
 alert("Thank you for playing");
+}
+if(truthordare == true){
+var random = Math.floor(Math.random() * 11) + 1;
+alert(random);
+}
+if(truthordare == false){
+var randomtruth = Math.floor(Math.random() * 21) + 11;
+alert(randomtruth);
 }
 if(random == 1){
 
@@ -35,5 +42,35 @@ if(random == 9){
 	 
 }
 if(random == 10){
+	 
+}
+if(random == 11){
+
+}
+if(random == 12){
+	 
+}
+if(random == 13){
+	 
+}
+if(random == 14){
+	 
+}
+if(random == 15){
+	 
+}
+if(random == 16){
+	 
+}
+if(random == 17){
+	 
+}
+if(random == 18){
+	 
+}
+if(random == 19){
+	 
+}
+if(random == 20){
 	 
 }
