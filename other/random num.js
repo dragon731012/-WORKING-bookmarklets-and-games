08a6d112a47d507df044acee1962c69bb0a9fd6a
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2022-11-22 14:39:04 -0600

    Update and rename tests (don't work)/random num.js to other/random num.js

--- other/random num.js
+++ other/random num.js
@@ -1,4 +1,5 @@
 javascript:
+alert("by dragonmaster73101-dragonmaster731012 on github");
 l=prompt("what do you want the max to be? ex: 1-6 max:6");
 m=prompt("what do you want the min to be? ex: 1-6 min=1");
 function getRandomNumberBetween(min,max){ return Math.floor(Math.random()*(max-min+1)+min); };
