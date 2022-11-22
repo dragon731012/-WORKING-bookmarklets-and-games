52d0c4eeabde8a6516017dd4ccda24776e7ad6ee
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-22 14:37:39 -0600

    Update and rename random num to random num.js

---
+++ tests (don't work)/random num.js
@@ -0,0 +1,5 @@
+javascript:
+l=prompt("what do you want the max to be? ex: 1-6 max:6");
+m=prompt("what do you want the min to be? ex: 1-6 min=1");
+function getRandomNumberBetween(min,max){ return Math.floor(Math.random()*(max-min+1)+min); };
+a=getRandomNumberBetween(m,l);alert("your random number is "+a+".")
