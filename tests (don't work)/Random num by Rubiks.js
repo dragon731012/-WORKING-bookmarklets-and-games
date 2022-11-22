35d52b460a29b2e1f3d9d488c4baef9b782eaeac
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-22 14:46:34 -0600

    Delete Random num by Rubiks.js

--- tests (don't work)/Random num by Rubiks.js
+++
@@ -1,7 +0,0 @@
-javascript:
-l=prompt("what do you want the max to be? ex: 1-10 max:10");
-m=prompt("what do you want the min to be? ex: 1-10 min=1");
-x = l + 1
-y = m - 1
-a = Math.Floor(Math.random() * x) + y;
-alert("your random number is "+a+".");
