51c6a2a8d459fb3aed61f83b472e0bd02d7ff089
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-22 12:01:50 -0600

    Create Game1test.js

---
+++ tests (don't work)/Game1test.js
@@ -0,0 +1,39 @@
+javascript:
+var start = confrim("Would you like to play Untiteld Game");
+if(start == true){
+var random = Math.floor(Math.random() * 11);
+alert(random);
+}
+else{
+alert("Thank you for playing");
+}
+if(random == 1){
+
+}
+if(random == 2){
+	 
+}
+if(random == 3){
+	 
+}
+if(random == 4){
+	 
+}
+if(random == 5){
+	 
+}
+if(random == 6){
+	 
+}
+if(random == 7){
+	 
+}
+if(random == 8){
+	 
+}
+if(random == 9){
+	 
+}
+if(random == 10){
+	 
+}
