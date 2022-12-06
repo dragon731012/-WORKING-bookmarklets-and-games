7f57f61cd704d8e8d850e4b688bc7bdfab45eae9
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2022-12-06 12:10:00 -0600

    Update clock test.js

--- tests (don't work)/clock test.js
+++ tests (don't work)/clock test.js
@@ -1,1 +1,1 @@
-javascript:const date = new Date();var month=date.getMonth();var day=date.getDay();var mill=date.getMilliseconds();var sec=date.getSeconds();var hrs=date.getHours();var min=date.getMinutes();var yr=date.getFullYear();alert("the date is "+month+"/"+day+"/"+yr+".");
+javascript:const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];const d = new Date();let day=getDate();let dayweek = days[d.getDay()];alert("the date is "+month+"/"+day+"/"+yr+".");
