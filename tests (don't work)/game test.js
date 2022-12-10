5a6e66bc4ff6e1960f945424c10bfaf854f9c303
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2022-12-09 19:24:02 -0600

    Update game test.js

--- tests (don't work)/game test.js
+++ tests (don't work)/game test.js
@@ -1,6 +1,84 @@
 alert("by dragonmaster73101");
 (function() {
-	var green = document.createElement('div');
+	var me = document.createElement('div');
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(me);
+
+	me.style.position = 'fixed';
+	me.style.top = '215px';
+	me.style.left = '125px';
+	me.style.margin = '10px';
+	me.style.paddingTop = '10px';
+	me.style.width = '85px';
+	me.style.height = '40px';
+	me.style.zIndex = 10000;
+	me.style.opacity = 1;
+	me.style.color = 'black';
+	me.style.backgroundColor = 'gray';
+	me.style.border = '0px solid white';
+	me.style.textAlign = 'center';
+	me.style.cursor = 'pointer';
+	me.id = 'me';
+	me.style.display = 'block';
+	me.innerText = 'by dragonmaster73101';
+	me.style.fontSize = '8px';
+	document.getElementById('me').addEventListener('click', function() {
+		window.open('https://github.com/dragon731012');
+
+	});
+	var start = document.createElement('div');
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(start);
+
+	start.style.position = 'fixed';
+	start.style.top = '215px';
+	start.style.left = '0px';
+	start.style.margin = '10px';
+	start.style.paddingTop = '10px';
+	start.style.width = '115px';
+	start.style.height = '40px';
+	start.style.zIndex = 10000;
+	start.style.opacity = 1;
+	start.style.color = 'black';
+	start.style.backgroundColor = 'gray';
+	start.style.border = '0px solid white';
+	start.style.textAlign = 'center';
+	start.style.cursor = 'pointer';
+	start.id = 'start';
+	start.style.display = 'block';
+	start.innerText = 'start';
+	document.getElementById('start').addEventListener('click', function() {
+		start.style.left = '-999999px';
+		me.style.width = '210px';
+		me.style.left = '0px';
+		me.style.fontSize = '15px';
+		start = 1;
+		const list = [];
+		var x = 0;
+		var m = 1;
+		while (true) {
+			while (x < m) {
+				function getRandomNumberBetween(min, max) {
+					return Math.floor(Math.random() * (max - min + 1) + min);
+				}
+				var a = getRandomNumberBetween(1, 4);
+				list.push(a);
+				x = x + 1;
+			}
+
+			function allclick() {
+				if (allclick === false) {
+					window.setTimeout(allclick, 100); /* this checks the flag every 100 milliseconds*/
+				} else {
+					m = m + 1;
+				}
+			}
+			allclick();
+
+			m = m + 1;
+		}
+	});
+		var green = document.createElement('div');
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(green);
 	green.style.position = 'fixed';
@@ -109,83 +187,5 @@ alert("by dragonmaster73101");
 		setTimeout(function() {
 			blue.style.opacity = 0.8;
 		}, 500);
-	});
-	var me = document.createElement('div');
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(me);
-
-	me.style.position = 'fixed';
-	me.style.top = '215px';
-	me.style.left = '125px';
-	me.style.margin = '10px';
-	me.style.paddingTop = '10px';
-	me.style.width = '85px';
-	me.style.height = '40px';
-	me.style.zIndex = 10000;
-	me.style.opacity = 1;
-	me.style.color = 'black';
-	me.style.backgroundColor = 'gray';
-	me.style.border = '0px solid white';
-	me.style.textAlign = 'center';
-	me.style.cursor = 'pointer';
-	me.id = 'me';
-	me.style.display = 'block';
-	me.innerText = 'by dragonmaster73101';
-	me.style.fontSize = '8px';
-	document.getElementById('me').addEventListener('click', function() {
-		window.open('https://github.com/dragon731012');
-
-	});
-	var start = document.createElement('div');
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(start);
-
-	start.style.position = 'fixed';
-	start.style.top = '215px';
-	start.style.left = '0px';
-	start.style.margin = '10px';
-	start.style.paddingTop = '10px';
-	start.style.width = '115px';
-	start.style.height = '40px';
-	start.style.zIndex = 10000;
-	start.style.opacity = 1;
-	start.style.color = 'black';
-	start.style.backgroundColor = 'gray';
-	start.style.border = '0px solid white';
-	start.style.textAlign = 'center';
-	start.style.cursor = 'pointer';
-	start.id = 'start';
-	start.style.display = 'block';
-	start.innerText = 'start';
-	document.getElementById('start').addEventListener('click', function() {
-		start.style.left = '-999999px';
-		me.style.width = '210px';
-		me.style.left = '0px';
-		me.style.fontSize = '15px';
-		start = 1;
-		const list = [];
-		var x = 0;
-		var m = 1;
-		while (true) {
-			while (x < m) {
-				function getRandomNumberBetween(min, max) {
-					return Math.floor(Math.random() * (max - min + 1) + min);
-				}
-				var a = getRandomNumberBetween(1, 4);
-				list.push(a);
-				x = x + 1;
-			}
-
-			function allclick() {
-				if (allclick === false) {
-					window.setTimeout(allclick, 100); /* this checks the flag every 100 milliseconds*/
-				} else {
-					m = m + 1;
-				}
-			}
-			allclick();
-
-			m = m + 1;
-		}
 	});
 }());
