142e3f88e3206e5c0fc542adc3e8223234d1d6b9
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2022-12-10 10:10:15 -0600

    Update painting test.js

--- tests (don't work)/painting test.js
+++ tests (don't work)/painting test.js
@@ -1,33 +1,3 @@
-function mousemove(event){
-     var x=event.clientX;
-     var y=event.clientY;
-     x=x-10;
-     y=y-8;
-(function () {
-	var elem = document.createElement('div');
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(elem);
-
-	elem.style.position = 'fixed';
-	elem.style.top = ''+y+'px';
-	elem.style.left =''+x+'px';
-	elem.style.margin = '10px';
-	elem.style.paddingTop = '10px';
-	elem.style.width = '10px';
-	elem.style.height = '10px';
-	elem.style.zIndex = 10000;
-	elem.style.opacity = 1;
-	elem.style.color = 'blue';
-	elem.style.backgroundColor = 'blue';
-	elem.style.border = '0px solid white';
-	elem.style.textAlign = 'center';
-	elem.id = 'paint';
-	elem.style.display = 'circle';
-	elem.innerText = '';
-
-}());
-}
-
 (function () {
 	var color = document.createElement('div');
 	var body = document.getElementsByTagName('body')[0];
@@ -53,5 +23,38 @@ function mousemove(event){
 	color.innerText = '';
 
 }());
+
+function mousemove(event){
+     var x=event.clientX;
+     var y=event.clientY;
+     x=x-10;
+     y=y-8;
+(function () {
+	if (color.mouseenter=false){alert("hi");
+	var elem = document.createElement('div');
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(elem);
+
+	elem.style.position = 'fixed';
+	elem.style.top = ''+y+'px';
+	elem.style.left =''+x+'px';
+	elem.style.margin = '10px';
+	elem.style.paddingTop = '10px';
+	elem.style.width = '10px';
+	elem.style.height = '10px';
+	elem.style.zIndex = 10000;
+	elem.style.opacity = 1;
+	elem.style.color = 'blue';
+	elem.style.backgroundColor = 'blue';
+	elem.style.border = '0px solid white';
+	elem.style.textAlign = 'center';
+	elem.id = 'paint';
+	elem.style.display = 'circle';
+	elem.innerText = '';
+	}
+
+}());
+}
+
 
 window.addEventListener('mousemove', mousemove);
