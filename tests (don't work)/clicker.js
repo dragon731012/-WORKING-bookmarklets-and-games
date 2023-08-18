27b5c1ce0df1faf3e2b0c2eac9463788be542b48
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-08-18 19:51:51 +0000

    Update clicker.js

--- tests (don't work)/clicker.js
+++ tests (don't work)/clicker.js
@@ -1,4 +1,4 @@
 var click = document.createElement('div');
 var body = document.getElementsByTagName('body')[0];
-click.style.background="url(https://static.wikia.nocookie.net/cookieclicker/images/5/5a/PerfectCookie.png)";
+click.style.backgroundImage="url(https://static.wikia.nocookie.net/cookieclicker/images/5/5a/PerfectCookie.png)";
 body.appendChild(click);
