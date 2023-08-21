7b7facc88e280d8e699e8c01476115c7cbbbda99
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-08-21 12:52:22 +0000

    Update clicker.js

--- tests (don't work)/clicker.js
+++ tests (don't work)/clicker.js
@@ -1,6 +1,8 @@
-var click = document.createElement('div');
-var body = document.getElementsByTagName('body')[0];
-body.appendChild(click);
-click.id="click";
-function bg(){click.style.backgroundImage="url(https://static.wikia.nocookie.net/cookieclicker/images/5/5a/PerfectCookie.png)";}
-bg();
+(function () {
+  var click = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(click);
+  click.id="click";
+  function bg(){click.style.backgroundImage="url(https://static.wikia.nocookie.net/cookieclicker/images/5/5a/PerfectCookie.png)";}
+  bg();
+}());
